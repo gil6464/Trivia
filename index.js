094e4ff6a4b4f6ea7ed026c75b1e9f64b541458f
gil6464 <gil6464@gmail.com>
2021-04-22 21:58:00 +0300

    started queries

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const { countryMain } = require("./models");
+const { countryMain, questiontypeone, questiontypetwo } = require("./models");
+const Sequelize = require("Sequelize");
+const sequelize = require("sequelize");
 const { Op } = require("sequelize");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const router = express.Router();
 
 // Get country that name is Switzerland
 // countryMain.findOne({ where: { country: "Switzerland" } }).then(country => {
@@ -7,26 +14,49 @@ const { Op } = require("sequelize");
 // });
 
 // Get country that GDP > 35000
-countryMain.findAll({ where: { GDP: { [Op.gte]: 35000 } } }).then(countries => {
-  console.log(countries.map(country => console.log(country.toJSON())));
+const answar = async () => {
+  await countryMain
+    .findAll({ where: { GDP: { [Op.gte]: 35000 } } })
+    .then(countries => {
+      return countries.map(country => country.toJSON());
+    });
+};
+
+app.get("/question/typeone", (req, res) => {
+  const questions = questiontypeone
+    .findOne({ where: { id: Math.floor(Math.random() * 10) + 1 } })
+    .then(question => {
+      res.send(question.toJSON());
+    });
 });
 
-function createQuestion() {
-  let country1 = getRandomCountry();
-  let country2 = getRandomCountry();
-  if (country1 === country2) {
-    //If they are the same then shuffle them both
-    country1 = getRandomCountry();
-    country2 = getRandomCountry();
-  }
+// countryMain.findAll({ order: Sequelize.literal('rand()'), limit: 5 }).then((countries) => {
+//   // single random encounter
+// });
 
-  let question = {};
-  question.firstCountry = country1;
-  question.secondCountry = country2;
-}
+// order: [["price", "DESC"]],
+//   limit: 1,
+// { order: Sequelize.literal('rand()'), limit: 4 }
+// function createQuestion() {
+//   let country1 = getRandomCountry();
+//   let country2 = getRandomCountry();
+//   if (country1 === country2) {
+//     //If they are the same then shuffle them both
+//     country1 = getRandomCountry();
+//     country2 = getRandomCountry();
+//   }
 
-function getRandomCountry() {
-  // get
-  //todo get random country function
-  return "";
-}
+//   let question = {};
+//   question.firstCountry = country1;
+//   question.secondCountry = country2;
+// }
+
+// function getRandomCountry() {
+//   // get
+//   //todo get random country function
+//   return "";
+// }
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!");
+});
