1dfb8ccc1f214f5752ed4635d26fc10471144f15
gil6464 <gil6464@gmail.com>
2021-04-22 19:07:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ const { Op } = require("sequelize");
 countryMain.findAll({ where: { GDP: { [Op.gte]: 35000 } } }).then(countries => {
   console.log(countries.map(country => console.log(country.toJSON())));
 });
+
+function createQuestion() {
+  let country1 = getRandomCountry();
+  let country2 = getRandomCountry();
+  if (country1 === country2) {
+    //If they are the same then shuffle them both
+    country1 = getRandomCountry();
+    country2 = getRandomCountry();
+  }
+
+  let question = {};
+  question.firstCountry = country1;
+  question.secondCountry = country2;
+}
+
+function getRandomCountry() {
+  // get
+  //todo get random country function
+  return "";
+}
