5c80590c8b6dacb46fec0b428e56982281f8f3d4
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-05-01 19:50:33 +0300

    added table

--- Client/trivia-app/src/components/Home/First.js
+++ Client/trivia-app/src/components/Home/First.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router";
+import logo from "./logo.png";
 
 function First(props) {
   const [toLogin, setToLogin] = useState(false);
   const [toSignUp, setToSignUp] = useState(false);
+
+  const image = <img src={logo} alt="question-logo" id="question-button" />;
+  const tableRow = (
+    <tr>
+      <td>{image}</td>
+      <td>{image}</td>
+      <td>{image}</td>
+      <td>{image}</td>
+    </tr>
+  );
+
   return (
     <div className="loginAndSignup">
       {toLogin && <Redirect to="/login"></Redirect>}
